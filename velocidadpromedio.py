1f7401915af470369eb0e01766bc3b604584dc06
ericjardon <46682769+ericjardon@users.noreply.github.com>
2019-01-21 09:41:50 -0600

    Eric Jardón Chao, Grupo 02

---
+++ velocidadpromedio.py
@@ -0,0 +1,9 @@
+# Autor: Eric Jardón Chao
+# Calcular la velocidad promedio
+
+distancia=int(input("Rregistra la distancia recorrida en km: "))
+tiempo=int(input("Registra el tiempo transcurrido en horas: "))
+
+velocidad=distancia/tiempo
+
+print("La velocidad promedio fue de: ",velocidad,"km/h")
